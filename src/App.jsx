95b092a709ab3ec6d62d251641b8e9eb217b62d5
gaurav0277 <gauravkumarpal9368@gmail.com>
2025-05-20 11:46:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Welcome to the job-board</h1>
+      <p>This is the begginning of my rect repo</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar() {
+    return(
+        <div>
+            <h1>--------------this is an navbar--------</h1>
+        </div>
+    );
+}
+export default Navbar;
